6d4f0719b3e60b1c17e97dcb6c5d0801dfd4faf8
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-02-22 01:27:27 -0500

    Initial work toward replacing the CompilerFlasher elements with direct calls to the Builder API. Adds Silex and Guzzle integrations and presents the Blockly interface through the server root address. Cleans up the HTML/CSS/JavaScript of the main page. Does not pass code between the JavaScript and the PHP just yet, though.

--- server_quickstart/quickstart.sh
+++ server_quickstart/quickstart.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
 # Handy variables!
+OURUSER=${1:-ubuntu}
 WEBROOT=/var/www
 BDROOT=$WEBROOT/BlocklyDuino
 NGINXROOT=/etc/nginx
+COMPOSERDIR=$HOME
 
 # Download programs, etc.
 echo "[Step 1]: Installing updates and software..."
 sudo apt-get -y update && \
 sudo apt-get -y upgrade && \
-sudo apt-get -y install nginx git
+sudo apt-get -y install python-software-properties curl nginx git && \
+sudo apt-get -y install apt-get install php5-fpm php-cli php5-curl
+cd $COMPOSERDIR
+wget https://getcomposer.org/composer.phar
 
-# Backup files that we'll overwrite, just in case.
 echo "[Step 2]: Backing up default files..."
 mkdir $HOME/backups
 sudo cp $NGINXROOT/nginx.conf $HOME/backups/.
 sudo cp $NGINXROOT/sites-available/default $HOME/backups/.
+sudo cp /etc/php5/fpm/pool.d/www.conf $HOME/backups/.
 
-# Create a user/group for nginx.
 echo "[Step 3]: Setting up Nginx user and group..."
 sudo groupadd -g 80 nginx
 sudo useradd -M -d /dev/null -s /bin/false -u 80 -g nginx nginx
+# As part of this, we need to update the php5-fpm user and group
+sudo sed "s/nginx/www-data/g" $HOME/backups/www.conf > /etc/php5/fpm/pool.d/www.conf
+sudo service php5-fpm restart
 
-# Create www directory and clone our git repo in there.
 echo "[Step 4]: Creating a web directory and downloading the BlocklyDuino git repo..."
 sudo mkdir -p $WEBROOT
 cd $WEBROOT
@@ -30,20 +36,19 @@ sudo git clone https://github.com/codebendercc/BlocklyDuino.git
 cd $BDROOT
 
 # The next line will need to be removed once this gets onto the master branch
-sudo git checkout server_quickstart
+sudo git checkout builder_service_integration
 
 echo "[Step 5]: Applying configuration files..."
-# Copy the config file into /etc/nginx/nginx.conf
 sudo cp $BDROOT/server_quickstart/nginx.conf $NGINXROOT/nginx.conf
+sudo cp $BDROOT/server_quickstart/blocklyduino.conf $NGINXROOT/sites-available/default
 
-# Update the /etc/nginx/sites-available/default file to point to where we'll download the repo
-sudo cp $BDROOT/server_quickstart/default_site $NGINXROOT/sites-available/default
+echo "[Step 6]: Running Composer..."
+sudo php $COMPOSERDIR/composer.phar update
 
-echo "[Step 6]: Fixing up permissions and restarting the Nginx service..."
+echo "[Step 7]: Fixing up permissions and restarting the Nginx service..."
 # Finally, make sure everything has the appropriate group, and bounce the nginx server
-sudo chown -R ubuntu:nginx $WEBROOT
-sudo service nginx stop
-sudo service nginx start
+sudo chown -R $OURUSER:nginx $WEBROOT
+sudo service nginx restart
 
-echo "[Step 7]: ..."
+echo "[Step 8]: ..."
 echo "OOOOOOOH YEEEEEEYAH!"
