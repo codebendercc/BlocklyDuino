dbea713745248dd7136e20935fb110f1e843ca02
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-03-04 18:26:00 -0500

    Tidies up the application structure. The /compile route is more or less ready.

--- src/beans/BuilderRequest.php
+++ src/beans/BuilderRequest.php
@@ -26,12 +26,13 @@ class BuilderRequest implements \JsonSerializable
         $this->logging = true;
         $this->format = "binary";
         $this->version = "105";
-        $this->build = [
-            'mcu' => 'atmega328p',
-            'f_cpu' => '16000000L',
-            'core' => 'arduino',
-            'variant' => 'standard'
-        ];
+//        $this->build = [
+//            'mcu' => 'atmega328p',
+//            'f_cpu' => '16000000L',
+//            'core' => 'arduino',
+//            'variant' => 'standard'
+//        ];
+        $this->build = new BuildData();
     }
 
     /**
@@ -105,7 +106,173 @@ class BuilderRequest implements \JsonSerializable
             'logging' => $this->logging,
             'format' => $this->format,
             'version' => $this->version,
-            'build' => $this->build
+            'build' => $this->build->toArray()
+            //'build' => $this->build
         );
+    }
+}
+
+class BuildData implements \JsonSerializable {
+
+    protected $mcu;
+    protected $f_cpu;
+    protected $core;
+    protected $variant;
+    protected $vid;
+    protected $pid;
+
+
+
+    // TODO: Refactor this when we're ready to pass in these values
+    function __construct(){
+        $this->mcu = 'atmega328p';
+        $this->f_cpu = '16000000L';
+        $this->core = 'arduino';
+        $this->variant = 'standard';
+    }
+
+    /**
+     * Processor's MCU spec
+     * @return string
+     */
+    public function getMcu()
+    {
+        return $this->mcu;
+    }
+
+    /**
+     * Sets processor's MCU spec
+     * @param string $mcu
+     */
+    public function setMcu($mcu)
+    {
+        $this->mcu = $mcu;
+    }
+
+    /**
+     * Processor's clock speed F_CPU spec
+     * @return string
+     */
+    public function getFCpu()
+    {
+        return $this->f_cpu;
+    }
+
+    /**
+     * Sets processor's clock speed F_CPU spec
+     * @param string $f_cpu
+     */
+    public function setFCpu($f_cpu)
+    {
+        $this->f_cpu = $f_cpu;
+    }
+
+    /**
+     * Arduino core used for the board
+     * @return string
+     */
+    public function getCore()
+    {
+        return $this->core;
+    }
+
+    /**
+     * Sets Arduino core used for the board
+     * @param string $core
+     */
+    public function setCore($core)
+    {
+        $this->core = $core;
+    }
+
+    /**
+     * Variant used for the board
+     * @return string
+     */
+    public function getVariant()
+    {
+        return $this->variant;
+    }
+
+    /**
+     * Sets variant used for the board
+     * @param string $variant
+     */
+    public function setVariant($variant)
+    {
+        $this->variant = $variant;
+    }
+
+    /**
+     * USB VID (optional, for Leonardo-like USB-enabled devices)
+     * @return string
+     */
+    public function getVid()
+    {
+        return $this->vid;
+    }
+
+    /**
+     * Sets USB VID (optional, for Leonardo-like USB-enabled devices)
+     * @param string $vid
+     */
+    public function setVid($vid)
+    {
+        $this->vid = $vid;
+    }
+
+    /**
+     * USB VID (optional, for Leonardo-like USB-enabled devices)
+     * @return string
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    /**
+     * Sets USB VID (optional, for Leonardo-like USB-enabled devices)
+     * @param string $pid
+     */
+    public function setPid($pid)
+    {
+        $this->pid = $pid;
+    }
+
+    /**
+     * The BuilderRequest as an array
+     * @return array this
+     */
+    function toArray()
+    {
+        $arr = [
+            'mcu' => $this->mcu,
+            'f_cpu' => $this->f_cpu,
+            'core' => $this->core,
+            'variant' => $this->variant
+        ];
+
+        // $pid and $vid are optional parameters
+        if($this->pid != null) {
+            $arr['pid'] = $this->pid;
+        }
+
+        if($this->vid != null) {
+            $arr['vid'] = $this->vid;
+        }
+
+        return $arr;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
