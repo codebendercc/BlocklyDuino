839f37b070a9d1948af01ad574d259b79bcb1d54
azyth <jh836@cornell.edu>
2015-03-04 21:22:00 -0500

    turns event manager on

--- public/blocklyflasher.js
+++ public/blocklyflasher.js
@@ -1,84 +1,90 @@
 blocklyflasher = function(code){
 
-//// The Event Manager registers any events that we know of
-//    this.eventManager = new function(){
-//        this._listeners = {};
-//
-//        // Adds a Listener that responds to a given type of event
-//        this.addListener = function(type, listener){
-//            if (typeof this._listeners[type] == "undefined"){
-//                this._listeners[type] = [];
-//            }
-//            this._listeners[type].push(listener);
-//        };
-//
-//        // Fire an event, notifying all known Listeners
-//        this.fire = function(event, param1, param2){
-//            if (typeof event == "string"){
-//                event = { type: event };
-//            }
-//            if (!event.target){
-//                event.target = this;
-//            }
-//            if (!event.type){
-//                throw new Error("Event object missing 'type' property.");
-//            }
-//            if (this._listeners[event.type] instanceof Array){
-//                var listeners = this._listeners[event.type];
-//                for (var i=0, len=listeners.length; i < len; i++){
-//                    if(typeof param1 !== 'undefined')
-//                    {
-//                        if(typeof param2 !== 'undefined')
-//                        {
-//                            listeners[i].call(this, param1, param2);
-//                        }
-//                        else
-//                        {
-//                            listeners[i].call(this, param1);
-//                        }
-//                    }
-//                    else
-//                    {
-//                        listeners[i].call(this);
-//                    }
-//                }
-//            }
-//        };
-//
-//        // Unregister a Listener
-//        this.removeListener = function(type, listener){
-//            if (this._listeners[type] instanceof Array){
-//                var listeners = this._listeners[type];
-//                for (var i=0, len=listeners.length; i < len; i++){
-//                    if (listeners[i] === listener){
-//                        listeners.splice(i, 1);
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//    };
-//
-//    // Defines where to put the output if there is any
-//    this.setOperationOutput = function(message){
-//        // TODO: Change the "cf" in this ID to "bf"
-//        $("#cb_cf_operation_output").html(message);
-//    }
-//
-//    // Shorthand for adding a Listener to the Event Manager
-//    this.on = function(type, listener){
-//        this.eventManager.addListener(type, listener);
-//    }
-//
-//    this.on("pre_verify", this.disableCompilerFlasherActions);
-//    this.on("verification_succeed", this.enableCompilerFlasherActions);
-//    this.on("verification_failed", this.enableCompilerFlasherActions);
-//    this.on("pre_flash", this.disableCompilerFlasherActions);
-//    this.on("flash_failed", this.enableCompilerFlasherActions);
-//    this.on("flash_succeed", this.enableCompilerFlasherActions);
-//    this.on("pre_hex", this.disableCompilerFlasherActions);
-//    this.on("hex_succeed", this.enableCompilerFlasherActions);
-//    this.on("hex_failed", this.enableCompilerFlasherActions);
+// The Event Manager registers any events that we know of
+    this.eventManager = new function(){
+        this._listeners = {};
+
+        // Adds a Listener that responds to a given type of event
+        this.addListener = function(type, listener){
+            if (typeof this._listeners[type] == "undefined"){
+                this._listeners[type] = [];
+            }
+            this._listeners[type].push(listener);
+        };
+
+        // Fire an event, notifying all known Listeners
+        this.fire = function(event, param1, param2){
+            if (typeof event == "string"){
+                event = { type: event };
+            }
+            if (!event.target){
+                event.target = this;
+            }
+            if (!event.type){
+                throw new Error("Event object missing 'type' property.");
+            }
+            if (this._listeners[event.type] instanceof Array){
+                var listeners = this._listeners[event.type];
+                for (var i=0, len=listeners.length; i < len; i++){
+                    if(typeof param1 !== 'undefined')
+                    {
+                        if(typeof param2 !== 'undefined')
+                        {
+                            listeners[i].call(this, param1, param2);
+                        }
+                        else
+                        {
+                            listeners[i].call(this, param1);
+                        }
+                    }
+                    else
+                    {
+                        listeners[i].call(this);
+                    }
+                }
+            }
+        };
+
+        // Unregister a Listener
+        this.removeListener = function(type, listener){
+            if (this._listeners[type] instanceof Array){
+                var listeners = this._listeners[type];
+                for (var i=0, len=listeners.length; i < len; i++){
+                    if (listeners[i] === listener){
+                        listeners.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    // Defines where to put the output if there is any
+    this.setOperationOutput = function(message){
+        // TODO: Change the "cf" in this ID to "bf"
+        $("#cb_cf_operation_output").html(message);
+    }
+
+    // Shorthand for adding a Listener to the Event Manager
+    this.on = function(type, listener){
+        this.eventManager.addListener(type, listener);
+    }
+
+    this.on("pre_verify", this.disableCompilerFlasherActions);
+    this.on("verification_succeed", this.enableCompilerFlasherActions);
+    this.on("verification_failed", this.enableCompilerFlasherActions);
+    this.on("pre_flash", this.disableCompilerFlasherActions);
+    this.on("flash_failed", this.enableCompilerFlasherActions);
+    this.on("flash_succeed", this.enableCompilerFlasherActions);
+    this.on("pre_hex", this.disableCompilerFlasherActions);
+    this.on("hex_succeed", this.enableCompilerFlasherActions);
+    this.on("hex_failed", this.enableCompilerFlasherActions);
+
+    if (verification_success){
+        this.eventManager.fire("verification_succeed");
+    }else{
+        this.eventManager.fire("verification_failed");
+    }
 
 //Events to handle
 //compilerflasher.on(eventName, callback);
@@ -140,7 +146,7 @@ var response = $.post( "/compile", function( code ) {
 //        "size":"2758",
 //        "output":"HUGE STRING HERE WITH BASE64 ENCODING OF BINARY AMMAGAAAAAD!!!!!11111oneoneonecos(0)=="
 //    }
-  var  verification_success = response.success;
+var verification_success = response.success;
 
 
 
