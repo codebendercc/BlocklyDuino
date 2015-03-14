b620dd0a0c495d8038cd49b8ac94869a2bc2e9a7
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-03-13 20:41:04 -0400

    #6 Migrate from compilerflasher to using codebender's builder service

---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+// index.php
+require __DIR__ . '/../app/app.php';
---
+++ src/services/CodebenderServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+// CodebenderServiceProvider.php
+
+namespace codebender\blocklyduino\services;
+
+use GuzzleHttp\Client;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class CodebenderServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @var string The URL for the Codebender website
+     */
+    protected $codebender_url = 'https://codebender.cc';
+
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Application $app
+     */
+    public function register(Application $app) {
+
+        /**
+         * Add closures for getting all the Codebender URLs we need to know.
+         */
+        $app['codebender'] = $app->protect(function ($uri) {
+            return sprintf('%s/%s', $this->codebender_url, ltrim($uri, '/'));
+        });
+        $app['codebender.board'] = $app->protect(function ($uri) use ($app) {
+            return sprintf('%s/board/%s', $this->codebender_url, ltrim($uri, '/'));
+        });
+        $app['codebender.utilities'] = $app->protect(function ($uri) use ($app) {
+            return sprintf('%s/utilities/%s', $this->codebender_url, ltrim($uri, '/'));
+        });
+
+        /**
+         * Add closures for performing requests against Codebender URLs.
+         */
+        $app['codebender.get'] = $app->protect(function ($url) use ($app) {
+            $client = new Client();
+            $response = $client->get($url);
+            return $response->getBody();
+        });
+
+        $app['codebender.post'] = $app->protect(function ($url, $json) use ($app) {
+            $client = new Client();
+            $response = $client->post($url, ['body' => $json]);
+            return $response->getBody();
+        });
+    }
+
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     *
+     * @param Application $app
+     */
+    public function boot(Application $app) {
+    }
+}
