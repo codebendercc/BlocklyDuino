5ed59bda45d67cb53ed662498432ea7dff88d044
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-04-27 23:24:02 -0400

    Adds a simple set of unit tests that verifies if the Builder service works as expected.

--- tests/unit/loading/PostTest.php
+++ tests/unit/loading/PostTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: azyth
- * Date: 4/27/15
- * Time: 11:33 AM
- */
 
 namespace codebender\blocklyduino\tests\unit\loading;
 
@@ -12,26 +6,11 @@ use codebender\blocklyduino\tests\unit\BlocklyduinoTestCase;
 
 class PostTest extends BlocklyduinoTestCase {
 
-    /**
-     *
-     */
     public function testPublishTwigPaths() {
         $client = $this->createClient();
 
         $this->markTestIncomplete('Silex publish correctly.');
         $this->markTestIncomplete("Dependencies::addBlocklyduinioAssets");
-    }
-
-    public function testBuilderCompiler() {
-
-        $this->markTestIncomplete("post to compiler and check response");
-    }
-
-    public function testApp(){
-
-        $this->markTestIncomplete('$app["codebender.get"]');
-        $this->markTestIncomplete('$app["codebender"]');
-        $this->markTestIncomplete('$app["codebender.builder"]');
     }
 
 }
